abfd151b7aabb94937dfd2a9fd0c6c423302daa4
Tom Kelly <tom@indigofair.com>
2019-01-22 10:21:36 -0500

    Nicer messages

--- balance.py
+++ balance.py
@@ -1,19 +1,19 @@
-a = float(raw_input('canadian index'))
-b = float(raw_input('us index'))
-c = float(raw_input('canadian bond index'))
-d = float(raw_input('international index'))
+a = float(raw_input('What is the market value of your Canadian Index holding? '))
+b = float(raw_input('What is the market value of your US Index holding? '))
+c = float(raw_input('What is the market value of your Canadian Bond Index holding? '))
+d = float(raw_input('What is the market value of your International Index holding? '))
 
-depositing = float(raw_input('depositing?'))
+depositing = float(raw_input('How much are you depositing? '))
 
 current_total = a + b + c + d
 desired_total = current_total + depositing
 
-desired_per = desired_total / 4
+desired_per = float(desired_total / 4)
 
 print('You want ${} per fund'.format(desired_per))
 print('That means deposit the following... ')
 
-print('- ${} into canadian index'.format(desired_per - a))
-print('- ${} into us index'.format(desired_per - b))
-print('- ${} into canadian bond index'.format(desired_per - c))
-print('- ${} into international index'.format(desired_per - d))
+print('- ${} into canadian index'.format(int(desired_per - a)))
+print('- ${} into us index'.format(int(desired_per - b)))
+print('- ${} into canadian bond index'.format(int(desired_per - c)))
+print('- ${} into international index'.format(int(desired_per - d)))
